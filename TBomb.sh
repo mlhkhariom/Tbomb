12e2422162be752544efa97966824859323d6f0e
mlhkinfotech <62866892+mlhkinfotech@users.noreply.github.com>
2020-07-13 00:11:56 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -14,7 +14,7 @@ echo .
 apt install figlet toilet python curl -y
 apt install python3-pip
 pip install -r requirements.txt
-echo This Script Was Made By SpeedX >update.speedx
+echo This Script Was Made By MLHK infotech >update.speedx
 echo Requirements Installed....
 echo Press Enter To Continue...
 read upd
@@ -27,15 +27,15 @@ echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
 toilet -f mono12 -F border SpeedX
-echo -e "\e[4;34m This Bomber Was Created By SpeedX \e[0m"
+echo -e "\e[4;34m This Bomber Was Created By MLHK infotech \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: mlhkofficial@gmail.com \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/mlhkhariom \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
 echo " "
-echo "Press 1 To  Start SMS Bomber "
-echo "Press 2 To  Start Call Bomber "
+echo "Press 1 To  Start SMS Bomber - SMS bhejo "
+echo "Press 2 To  Start Call Bomber - Call kro "
 echo "Press 3 To  Update (Works On Linux And Linux Emulators) "
 echo "Press 4 To  View Features "
 echo "Press 5 To  Exit "
@@ -50,7 +50,7 @@ exit 0
 elif [ $ch -eq 2 ];then
 clear
 echo -e "\e[1;32m"
-echo 'Call Bomb By SpeedX'> call.xxx
+echo 'Call Bomb By MLHK infotech'> call.xxx
 python3 bomber.py call
 rm *.xxx >/dev/null 2>&1
 exit 0
@@ -58,8 +58,8 @@ elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/TheSpeedX/TBomb
-if [[ -s TBomb/TBomb.sh ]];then
+git clone https://github.com/mlhkinfotech/mypro
+if [[ -s mypro/TBomb.sh ]];then
 cd TBomb
 cp -r -f * .. > temp
 cd ..
@@ -79,7 +79,7 @@ clear
 echo -e "\e[1;33m"
 figlet TBomb
 echo -e "\e[1;34mCreated By \e[1;34m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border MLHK infotech
 echo  " "
 echo -e "\e[1;32m                   Features\e[1;34m"
 echo "  [+] Unlimited And Super-Fast Bombing"
@@ -89,12 +89,12 @@ echo "  [+] Protection List"
 echo "  [+] Automated Future Updates"
 echo "  [+] Easy To Use And Embed in Code"
 echo -e "\e[1;32m                   Contributors\e[1;33m"
-echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
-echo "         [-] Mail At: ggspeedx29@gmail.com"
+echo -e "\e[1;33m      [*]  MLHK infotech   \e[1;31m"
+echo "         [-] Mail At: mlhkofficial@gmail.com"
 echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
-echo "         [-] Ping At: http://wa.me/917600140353"
+echo "         [-] Ping At: http://wa.me/916265433483"
 echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
-echo "         [-] Ping At: https://t.me/Rieltar"
+echo "         [-] Ping At: https://t.me/mlhkhariom"
 echo -e "\e[1;33m      [*]  0n1cOn3 (Stefan)   \e[1;31m"
 echo "         [-] Mail At: 0n1cOn3@gmx.ch"
 echo ""
@@ -106,9 +106,9 @@ echo -e "\e[1;32m Make Sure To Update it If It Does not Work.\e[0m"
 echo  " "
 echo -e "\e[4;31m That's All !!!\e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: mlhkofficial@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/mlhkinfotech \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/mlhkhariom \e[0m"
 echo "Press Enter To Go Home"
 read a3
 clear
@@ -117,11 +117,11 @@ clear
 echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border MLHK infotech
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-echo -e "\e[1;32m       Whatsapp: https://bit.do/speedxgit \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/GyanaTech \e[0m"
+echo -e "\e[1;32m           Mail: mlhkofficial@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: https://bit.do/mlhkinfotech \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/mlhkhariom \e[0m"
 echo " "
 exit 0
 else
